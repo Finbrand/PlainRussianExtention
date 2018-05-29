8a331b32097472a69c535b43d729d0a7b0c170a4
Finbrand <36167258+Finbrand@users.noreply.github.com>
2018-05-29 11:59:01 +0300

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,32 @@
+var CurrentURL = 'URL';
+var API="http://api.plainrussian.ru/api/1.0/ru/measure/?url=";
+
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    CurrentURL = tabs[0].url;
+	console.log (CurrentURL);
+	var Requst=API+CurrentURL;
+	console.log (Requst);
+	var requesturl = Requst;
+	console.log (requesturl);
+	var xhr = new XMLHttpRequest();
+//var APIurl = 'http://api.plainrussian.ru/api/1.0/ru/measure/';
+//var URL-sending= 'https://echo.msk.ru/blog/ssobyanin/2210036-echo/';
+//var requesturl = 'url=' + encodeURIComponent(URL-sending);
+xhr.open('POST', requesturl);
+xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+//xhr.responseType = 'json';
+xhr.send();	
+xhr.onreadystatechange = function () {
+	document.getElementById("State").innerHTML = xhr.readyState;
+	document.getElementById("Response").innerHTML = xhr.status;
+    if (xhr.readyState == 4 ) {
+        var index = JSON.parse(xhr.responseText);
+	document.getElementById("grade_SMOG").innerHTML = index.indexes.grade_SMOG;
+	    }
+};
+});
+//document.getElementById("CurrentURL").innerHTML = 'CurrentURL';
+//var requesturl = 'http://api.plainrussian.ru/api/1.0/ru/measure/?url=https://echo.msk.ru/blog/ssobyanin/2210036-echo/';
+
+//var requesturl = 'http://api.plainrussian.ru/api/1.0/ru/measure/?url='+CurrentURL;
+
