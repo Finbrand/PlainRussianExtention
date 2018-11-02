fec4671f5429409bc7fcb13afbe9429e3751f975
Finbrand <36167258+Finbrand@users.noreply.github.com>
2018-11-02 23:33:02 +0300

    Add files via upload
    02112018 added oreilly.ru server api

--- popup.js
+++ popup.js
@@ -1,5 +1,6 @@
 var CurrentURL = 'URL';
 var API="http://api.plainrussian.ru/api/1.0/ru/measure/?url=";
+var API22="http://api.oreilly.ru/?url=";
 
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     CurrentURL = tabs[0].url;
@@ -8,6 +9,8 @@ chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 	console.log (Requst);
 	var requesturl = Requst;
 	console.log (requesturl);
+	var Requst22=API22+CurrentURL;
+	console.log (Requst22);
 	var xhr = new XMLHttpRequest();
 //var APIurl = 'http://api.plainrussian.ru/api/1.0/ru/measure/';
 //var URL-sending= 'https://echo.msk.ru/blog/ssobyanin/2210036-echo/';
@@ -24,9 +27,22 @@ xhr.onreadystatechange = function () {
 	document.getElementById("grade_SMOG").innerHTML = index.indexes.grade_SMOG;
 	    }
 };
-});
 //document.getElementById("CurrentURL").innerHTML = 'CurrentURL';
 //var requesturl = 'http://api.plainrussian.ru/api/1.0/ru/measure/?url=https://echo.msk.ru/blog/ssobyanin/2210036-echo/';
 
 //var requesturl = 'http://api.plainrussian.ru/api/1.0/ru/measure/?url='+CurrentURL;
+var xhr22 = new XMLHttpRequest();
+xhr22.open('GET', Requst22);
+xhr22.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+//xhr.responseType = 'json';
+xhr22.send();	
+xhr22.onreadystatechange = function () {
+	document.getElementById("State22").innerHTML = xhr22.readyState;
+	document.getElementById("Response22").innerHTML = xhr22.status;
+    if (xhr22.readyState == 4 ) {
+        var index = JSON.parse(xhr22.responseText);
+	document.getElementById("grade_E2PI").innerHTML = index.indexes.grade_E2PI;
+	    }
+};
+});
 
